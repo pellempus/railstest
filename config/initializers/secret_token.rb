80411e24cffeb1f7ca1cb43673a8148940f888ec
Gokhan Demirel <gokhan@localhost.localdomain>
2013-12-26 04:17:18 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gogo::Application.config.secret_key_base = 'bdc159b72e37afe009b78ddcf063ca7c3bc1726193c9e471fcb204c9c169d2d41f231f39f3ebd1b2799ffa0523010ba62c17063cb60e9b2858b4d9c822e72028'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gogo::Application.config.session_store :cookie_store, key: '_gogo_session'
