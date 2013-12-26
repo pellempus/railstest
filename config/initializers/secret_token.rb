7b4bc3fdf9ddc73b303ef3b64d9b6b503cd44c8b
Gokhan Demirel <gokhan@localhost.localdomain>
2013-12-26 10:01:20 +0200

    Removed secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,16 @@
-# Be sure to restart your server when you modify this file.
+require 'securerandom'
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Gogo::Application.config.secret_key_base = 'bdc159b72e37afe009b78ddcf063ca7c3bc1726193c9e471fcb204c9c169d2d41f231f39f3ebd1b2799ffa0523010ba62c17063cb60e9b2858b4d9c822e72028'
+Gogo::Application.config.secret_key_base = secure_token
